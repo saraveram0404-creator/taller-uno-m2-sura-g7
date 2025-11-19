0c5f91e422be3ac94f40dcd43e40befbbda22271
sara vera <saraveram0404>
2025-11-19 02:20:18 -0500

    ejercicio 20

--- Sara Melissa Vera Mora/script.js
+++ Sara Melissa Vera Mora/script.js
@@ -154,6 +154,15 @@ tablero.fill("Casilla Vacía", 1, 5);
 console.log(tablero);
 
 
+//Ejer 20
+let codigos = ["A-10", "C-30", "B-20", "D-40"];
+
+codigos.sort();
+codigos.splice(2, 1); 
+codigos.unshift("X-99");
+codigos.reverse();
+
+console.log(codigos);
 
 
 
